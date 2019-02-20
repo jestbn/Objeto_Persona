ca44b6faba9ddc4112a99428233719d3118d5763
Juan Esteban Gil <juanestebang9@gmail.com>
2019-02-19 20:42:48 -0500

    Agregar archivos de proyecto.

---
+++ Objeto_Persona/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Objeto_Persona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
